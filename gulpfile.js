01276bf9d057c1443aeb39b3ec6abd7862b00be6
imagion <algernoncowperwood@gmail.com>
2017-09-02 03:21:04 +0300

    update

--- gulpfile.js
+++ gulpfile.js
@@ -1,24 +1,21 @@
-var gulp            = require('gulp'),
-	browserSync     = require('browser-sync'),
-	del             = require('del'),
-	cssnano         = require('gulp-cssnano'),
-	uglify          = require('gulp-uglify'),
-	rename          = require('gulp-rename'),
-	concat          = require('gulp-concat'),
-	imagemin        = require('gulp-imagemin'),
-	notify          = require("gulp-notify");
-	gutil           = require('gulp-util'),
-	cache           = require('gulp-cache'),
-	ftp             = require('vinyl-ftp'),
-
-	precss          = require('precss'),
-	postcss         = require('gulp-postcss'),
-	cssnext         = require('postcss-cssnext'),
-	fontMagician    = require('postcss-font-magician'),
-	short           = require('postcss-short'),
-	extend          = require('postcss-extend'),
-	mixins          = require('postcss-mixins'),
-	imports         = require('postcss-partial-import');
+var gulp            = require('gulp'),             // core
+	browserSync     = require('browser-sync'),     // refresh
+	del             = require('del'),              // delete files or folders
+	cssnano         = require('gulp-cssnano'),     // minify css
+	uglify          = require('gulp-uglify'),      // minify js
+	imagemin        = require('gulp-imagemin'),    // minify images
+	rename          = require('gulp-rename'),      // rename files
+	concat          = require('gulp-concat'),      // concat files
+	gutil           = require('gulp-util'),        // utils for plugins
+	cache           = require('gulp-cache'),       // clear cache or else
+	// plumber         = require('gulp-plumber'),     // error handler
+	sourcemaps      = require('gulp-sourcemaps'),  // sourcemaps
+	ftp             = require('vinyl-ftp'),        // upload files FTP
+	// последний precss не фурычит с последним postcss (16.08.17)
+	precss          = require('precss'),           // sass-like syntax
+	postcss         = require('gulp-postcss'),     // postcss core
+	cssnext         = require('postcss-cssnext'),  // css4 syntax
+	short           = require('postcss-short');    // shorthand css properties
 
 gulp.task('common-js', function() {
 	return gulp.src([
@@ -45,9 +42,6 @@ gulp.task('browser-sync', function() {
 		server: {
 			baseDir: 'app'
 		},
-		notify: false,
-		// tunnel: true,
-		// tunnel: "projectmane", //Demonstration page: http://projectmane.localtunnel.me
 	});
 });
 
@@ -55,31 +49,18 @@ gulp.task('css', function () {
 	var processors = [
 			precss,
 			cssnext,
-			fontMagician({
-				variants: {
-							'Roboto': {
-								'400': [],
-								'700': []
-							}
-						},
-						formats: 'woff ttf eot',
-						hosted: ['app/fonts/Roboto']
-			}),
 			short,
-			extend,
-			mixins,
-			imports
 	];
 	return gulp.src(['./app/init/main.css'])
 		.pipe(postcss(processors))
 		.pipe(rename({suffix: '.min', prefix : ''}))
 		.pipe(cssnano())
 		.pipe(gulp.dest('./app/css/'))
-		.pipe(browserSync.reload({stream: true}));
+		.pipe(browserSync.stream());
 });
 
 gulp.task('watch', ['css', 'js', 'browser-sync'], function() {
-	gulp.watch('app/sass/**/*.sass', ['sass']);
+	gulp.watch('app/init/**/*.css', ['css']);
 	gulp.watch(['libs/**/*.js', 'app/js/common.js'], ['js']);
 	gulp.watch('app/*.html', browserSync.reload);
 });
